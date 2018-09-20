dd7e1e00be2348c4472327d54e21e16e4faf13ee
Shibo Sun <shibos@student.unimelb.edu.au>
2018-09-20 15:21:23 +1000

    add set image from url

---
+++ SBImageView/UIImageView+SBImageView.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+SBImageView.h
+//  SBImageViewDemo
+//
+//  Created by 孙士博 on 2018/9/20.
+//  Copyright © 2018年 com.Instagram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (SBImageView)
+
+@end
---
+++ SBImageView/UIImageView+SBImageView.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+SBImageView.m
+//  SBImageViewDemo
+//
+//  Created by 孙士博 on 2018/9/20.
+//  Copyright © 2018年 com.Instagram. All rights reserved.
+//
+
+#import "UIImageView+SBImageView.h"
+
+@implementation UIImageView (SBImageView)
+
+@end
